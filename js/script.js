cacf86d6e9519c607211d85ed800403d25c0e15a
arsyadbruh <marsyad@student.ittelkom-sby.ac.id>
2021-10-13 13:52:18 +0700

    add sweetalert2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-var typed = new Typed('.typed', {
-    strings: ["Hai...", "My Name...", "Welcome Boy"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
+var typed = new Typed(".typed", {
+  strings: ["Hai...", "My Name...", "Welcome Boy"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
 });
+
+function yahalo() {
+  document.getElementById("player").play();
+  Swal.fire({
+    title: "ヾ(•ω•`)o",
+    timer: 1900,
+    width: "250px",
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    showConfirmButton: false,
+    showCancelButton: false,
+  });
+}
